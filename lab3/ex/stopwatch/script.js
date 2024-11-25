5ff43354e38d0380b884a0716e8a8cb75d50a1e8
Jakub Psarski <psarski@student.agh.edu.pl>
2024-11-25 22:10:01 +0100

    stopwatch done

--- lab3/ex/stopwatch/script.js
+++ lab3/ex/stopwatch/script.js
@@ -1,1 +1,54 @@
-let buttons = document.getElementsByClassName("button");
+let control = document.getElementById("control");
+let screen = document.getElementById("screen");
+let startButton = document.getElementById("start");
+let stopButton = document.getElementById("stop");
+let resetButton = document.getElementById("reset");
+
+let elapsedTime = 0;
+let timerInterval;
+let timerRunning = false;
+
+
+startButton.addEventListener("click", () => {
+    control.classList.add("active");
+    startTimer();
+});
+
+stopButton.addEventListener("click", () => {
+    control.classList.remove("active");
+    stopTimer();
+});
+
+resetButton.addEventListener("click", () => {
+	resetTimer();
+});
+
+function startTimer() {
+	if (!timerRunning) {
+		timerInterval = setInterval(() => {
+            elapsedTime++;
+            updateScreen();
+		}, 1000);
+		timerRunning = true;
+	}
+}
+
+function stopTimer() {
+	clearInterval(timerInterval);
+	timerRunning = false;
+}
+
+function resetTimer() {
+    elapsedTime = 0;
+    updateScreen();
+}
+
+function updateScreen() {
+    let minutes = Math.floor(elapsedTime / 60);
+    let seconds = elapsedTime % 60;
+    if (minutes > 0) {
+        screen.innerHTML = minutes + "min " + seconds + "s";
+    } else {
+        screen.innerHTML = seconds + "s";
+    }
+}
