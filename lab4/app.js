9313962cbbafb18c2996029fcefe5931000198dc
Jakub Psarski <psarski@student.agh.edu.pl>
2024-12-13 00:10:44 +0100

    userRoutes

--- lab4/app.js
+++ lab4/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import database from "./models/database.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/books", bookRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api", userRoutes);
 
 database.sync().then(() => {
 	app.listen(3000, () => {
