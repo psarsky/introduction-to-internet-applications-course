5fc2a797e36cb28f18087d3c7987748e39cf9174
Jakub Psarski <psarski@student.agh.edu.pl>
2025-01-10 20:32:55 +0100

    simplified server

--- mock-test/server/router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", async (_, res) => {
-	res.status(200).json({ color: "#0000FF" });
-});
-
-export default router;
--- mock-test/server/server.js
+++ mock-test/server/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import router from "./router.js";
-
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use("/", router);
+
+app.get("/", (_, res) => {
+	return res.json({ color: "blue" });
+});
 
 app.listen(3000, () => {
 	console.log("Server listening on port 3000");
